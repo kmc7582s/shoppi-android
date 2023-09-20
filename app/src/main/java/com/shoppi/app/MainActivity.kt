6bd8a7791bafeebcf7b7fa2029bb18b17b57d5d2
kmc7582s <kmc7582s@gmail.com>
2023-09-20 11:36:20 +0900

    section5 / Splash 화면 구성

--- app/src/main/java/com/shoppi/app/MainActivity.kt
+++ app/src/main/java/com/shoppi/app/MainActivity.kt
@@ -2,10 +2,15 @@ package com.shoppi.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_product_detail)
+        setContentView(R.layout.activity_main)
+
+        // 하단 네비게이션 메뉴 선택시 컬러 문제 해결
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.navigation_main)
+        bottomNavigationView.itemIconTintList = null
     }
 }
